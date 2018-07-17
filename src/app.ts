2244e03556f418b96a7ed1a53a06f9b957a27642
Flippie Coetser <flippie.coetser@gmail.com>
2018-07-17 19:51:39 +0200

    move Zoom and Pan example

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import Event from "./events";
 import * as d3 from "d3";
+import { Example } from "./example";
 
 let button = document.getElementById("trigger");
 let label = document.getElementById("label");
@@ -26,39 +27,6 @@ class Map extends HTMLElement {
 
 window.customElements.define("journey-map", Map);
 
-let w = 960;
-let h = 500;
+// tslint:disable-next-line:no-unused-variable
+let example = new Example();
 
-let data = d3.range(0, 100).map(function(d){
-  // tslint:disable-next-line:no-bitwise
-  return {"x": ~~(Math.random() * w ), "y": ~~(Math.random() * h), "r": ~~(Math.random() * 90) + 10 };
-});
-
-let svg = d3.select("#test")
-  .attr("width", w)
-  .attr("height", h);
-
-let zoomLayer  = svg.append("g");
-
-let color = d3.scaleOrdinal(d3.schemeCategory10);
-
-zoomLayer.selectAll("circle")
-  .data(data)
-  .enter()
-  .append("circle")
-  .attr("cx", function(d){ return d.x; })
-  .attr("cy", function(d){ return d.y; })
-  .attr("r", function(d){ return d.r; })
-  .attr("fill", function(d, i){
-    console.log(i);
-    return color(i.toString()); })
-  .attr("fill-opacity", 0.5);
-
-let zoomed = function() {
-  zoomLayer.attr("transform", d3.event.transform);
-};
-
-svg.call(d3.zoom()
-          .scaleExtent([1 / 2, 12])
-          .on("zoom", zoomed)
-    );
---
+++ src/example.ts
@@ -0,0 +1,51 @@
+import * as d3 from "d3";
+export class Example {
+    private width: number;
+    private hight: number;
+    private SVG;
+    private ZoomLayer;
+
+    constructor() {
+        this.width = 960;
+        this.hight = 500;
+
+        this.SVG = d3.select("#test")
+        .attr("width", this.width)
+        .attr("height", this.hight);
+
+        this.ZoomLayer  = this.SVG.append("g");
+
+        let color = this.color();
+
+        this.ZoomLayer.selectAll("circle")
+        .data(this.getData())
+        .enter()
+        .append("circle")
+        .attr("cx", function(d){ return d.x; })
+        .attr("cy", function(d){ return d.y; })
+        .attr("r", function(d){ return d.r; })
+        .attr("fill", function(d, i){
+            return color(i.toString());
+        })
+        .attr("fill-opacity", 0.5);
+
+        let zoomed = () => {
+            this.ZoomLayer.attr("transform", d3.event.transform);
+        };
+
+        this.SVG.call(d3.zoom()
+                .scaleExtent([1 / 2, 12])
+                .on("zoom", zoomed)
+            );
+    }
+    public getData() {
+        return d3.range(0, 100).map((d) => {
+            // tslint:disable-next-line:no-bitwise
+            return {"x": ~~(Math.random() * this.width ), "y": ~~(Math.random() * this.hight), "r": ~~(Math.random() * 90) + 10 };
+        });
+    }
+
+    public color() {
+        return d3.scaleOrdinal(d3.schemeCategory10);
+    }
+}
