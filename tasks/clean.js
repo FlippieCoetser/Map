0519edff9a51d71751e0dc4573d3d7aa1dca8149
FlippieCoetser <flippie.coetser@gmail.com>
2018-07-22 21:17:03 +0200

    Update Cucumber Report

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,6 +1,4 @@
 #! /usr/bin/env node
 let del = require('node-delete');
-del('./json', () => true);
-del('./report', () => true);
 del(['./src/*.js', './src/*.map', './src/*.d.ts'], () => true);
 del('./test/Unit/*.js', () => true);
